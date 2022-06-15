7c33918835f0010c041402a8b9b1162adcb18707
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2022-06-15 17:36:16 +0300

    Delete endpoint.

--- app/endpoints.py
+++ app/endpoints.py
@@ -1,4 +1,7 @@
+from uuid import UUID
+
 from fastapi import APIRouter
+from fastapi.params import Path
 from fastapi.responses import Response
 
 import schemas
@@ -8,5 +11,11 @@ router = APIRouter()
 
 @router.post("/imports")
 async def imports(shop_unit: schemas.ShopUnitImportRequest):
+    ...
+    return Response(status_code=200)
+
+
+@router.delete("/delete/{id}")
+async def delete(unit_id: UUID = Path(..., alias='id')):
     ...
     return Response(status_code=200)
