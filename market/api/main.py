72aa990e14220963b993ce8f30da47ec629fbd36
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2022-06-17 18:17:10 +0300

    db schema creation on startup & exception_handler

--- market/api/main.py
+++ market/api/main.py
@@ -1,5 +1,31 @@
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
+
 from market.api.endpoints import router
+from market.db.orm import Engine
+from market.db.model import Base
+from market.api import reponses
 
 app = FastAPI()
 app.include_router(router, tags=['MEGA MARKET API'])
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(*_):
+    """
+    Подменяет код и тело ответа при RequestValidationError на 400 и JSON
+    согласно описанию в openapi.yaml соответственно.
+    :return: market.api.schemas.Error
+    """
+    return reponses.VALIDATION_FAILED
+
+
+@app.on_event("startup")
+async def startup_event():
+    async with Engine.begin() as connection:
+        await connection.run_sync(Base.metadata.create_all)
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    await Engine.dispose()
