ca066b299ad2d826d67430f094c32c38013c21a5
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2022-06-18 03:15:10 +0300

    Adapt to new model + docs & comments.

--- market/api/handlers/imports.py
+++ market/api/handlers/imports.py
@@ -8,19 +8,24 @@ from market.db.orm import Session
 from market.db import model
 
 
-def prepare(unit_import: schemas.ShopUnitImportRequest):
+def prepare(unit_import: schemas.ShopUnitImportRequest) -> (list, list, list, dict):
     """
-    :raise ValidationFailed400:
-    :param unit_import:
+    Подготовка данных для вставки/обновления: категории и товары
+    разделяются на отдельные списки, создается общий список юнитов.
+    Кроме того собирается словарь uuid-тип для того чтобы в функции handle,
+    можно было проверить не изменяется ли тип уже существующего юнита.
+    :raise ValidationFailed400: в случае не валидных данный выбрасывается исключение.
     """
     if not unit_import.items:
         raise ValidationFailed400("empty list of units")
     categorise, offers, units, types = [], [], [], {}
     for unit in unit_import.items:
+        # Проверка уникальности uuid в рамках запроса.
         if unit.id in types:
             raise ValidationFailed400("duplicate uuid in a single request")
         types[unit.id] = unit.type
         if unit.type == 'CATEGORY':
+            # Проверка, что у категории отсутствует цена.
             if unit.price is not None:
                 raise ValidationFailed400("unit with the CATEGORY type cannot contain  a price field")
             categorise.append({
@@ -30,6 +35,7 @@ def prepare(unit_import: schemas.ShopUnitImportRequest):
                 'date': unit_import.update_date
             })
         elif unit.type == 'OFFER':
+            # Проверка, что у товара присутствует цена.
             if unit.price is None:
                 raise ValidationFailed400("unit with the OFFER type must contain a price field")
             offers.append({
@@ -39,12 +45,13 @@ def prepare(unit_import: schemas.ShopUnitImportRequest):
                 'date': unit_import.update_date,
                 'price': unit.price
             })
-        units.append({'uuid': unit.id, 'type': unit.type})
+        units.append({'uuid': unit.id, 'type': unit.type, 'parent_id': unit.parent_id})
     return categorise, offers, units, types
 
 
 async def handle(unit_import: schemas.ShopUnitImportRequest):
     """
+    Вставка и валидация данных, полученных от пользователя, в рамках одной транзакции.
     :raise ValidationFailed400:
     :param unit_import:
     """
@@ -52,18 +59,16 @@ async def handle(unit_import: schemas.ShopUnitImportRequest):
     async with Session() as s:
         async with s.begin():
             try:
+                # Получаем типы юнитов, которые ВОЗМОЖНО будем обновлять.
                 result = await s.execute(
                     select(model.ShopUnit).
                     where(model.ShopUnit.uuid.in_(types))
                 )
+                # Проверяем что не изменяется тип уже существующего юнита.
                 for u, in result.all():
                     if u.type != types[u.uuid]:
                         raise ValidationFailed400("it is not allowed to change the unit type")
-                await s.execute(
-                    insert(model.ShopUnit).
-                    values(units).
-                    on_conflict_do_nothing(index_elements=['uuid'])
-                )
+                # Вставка/обновление категорий.
                 if categorise:
                     category_insert_stmt = insert(model.Category).values(categorise)
                     await s.execute(
@@ -73,6 +78,16 @@ async def handle(unit_import: schemas.ShopUnitImportRequest):
                             set_=category_insert_stmt.excluded
                         ))
                     await s.execute(insert(model.CategoryHistory).values(categorise))
+                # Вставляем НОВЫЕ юниты.
+                # Вставка происходит ОБЯЗАТЕЛЬНО после категорий, иначе каталога юнита может
+                # ЕЩЁ НЕ БЫТЬ в базе данных в момент вставки его содержимого.
+                # (в случае, если в одной транзакции вставляются категории и их содержимое)
+                await s.execute(
+                    insert(model.ShopUnit).
+                    values(units).
+                    on_conflict_do_nothing(index_elements=['uuid'])  # Уже существующее не трогается.
+                )
+                # Вставка/обновление товаров.
                 if offers:
                     offer_insert_stmt = insert(model.Offer).values(offers)
                     await s.execute(
@@ -82,5 +97,7 @@ async def handle(unit_import: schemas.ShopUnitImportRequest):
                             set_=offer_insert_stmt.excluded
                         ))
                     await s.execute(insert(model.OffersHistory).values(offers))
+                # Исключения типа IntegrityError (или его наследники) возникают при ошибке целостности,
+                # а это означает, что данные пользователя невалидны.
             except (IntegrityError, ValidationFailed400) as err:
                 raise ValidationFailed400(err)
