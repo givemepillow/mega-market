28738ec11ebdd3cb607b8a19c6549c4329ebe2b9
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2022-06-23 20:09:17 +0300

    Recursive recalc & update in same query.

--- market/api/handlers/imports.py
+++ market/api/handlers/imports.py
@@ -2,17 +2,16 @@ from datetime import datetime
 from typing import List, Dict, Set, Tuple
 from uuid import UUID
 
-from sqlalchemy import select, update, bindparam, text
+from sqlalchemy import select, bindparam, text, update
 from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy.exc import IntegrityError
 
 from market.api import schemas
 from market.api.handlers.exceptions import ValidationFailed400
 from market.db.orm import Session
-from market.db import model
+from market.db import model, crud
 
-
-MAX_ITEMS = 6000
+MAX_ITEMS = 6500
 
 
 def prepare(unit_import: schemas.ShopUnitImportRequest) -> Tuple[List, List, List, Dict]:
@@ -23,14 +22,13 @@ def prepare(unit_import: schemas.ShopUnitImportRequest) -> Tuple[List, List, Lis
     можно было проверить не изменяется ли тип уже существующего юнита.
     :raise ValidationFailed400: в случае невалидных данный выбрасывается исключение.
     """
-    if not unit_import.items:
+    print("quantity: ", len(unit_import.items))
+    if len(unit_import.items) > MAX_ITEMS:
+        raise ValidationFailed400(f"to many items: {len(unit_import.items)}")
+    elif not unit_import.items:
         raise ValidationFailed400("empty list of units")
     categorise, offers, units, types = [], [], [], {}
-    quantity = 0
     for unit in unit_import.items:
-        if quantity > MAX_ITEMS:
-            raise ValidationFailed400("to many items")
-        quantity += 1
         # Проверка уникальности uuid в рамках запроса.
         if unit.id in types:
             raise ValidationFailed400("duplicate uuid in a single request")
@@ -44,6 +42,8 @@ def prepare(unit_import: schemas.ShopUnitImportRequest) -> Tuple[List, List, Lis
                 'parent_id': unit.parent_id,
                 'name': unit.name,
                 'date': unit_import.update_date,
+                'total_price': 0,
+                'offers_number': 0
             })
         elif unit.type == 'OFFER':
             # Проверка, что у товара присутствует цена.
@@ -70,16 +70,17 @@ async def parents(units: List[Dict], uuids: Dict, session: Session) -> Set[UUID]
     :param session: сессия БД.
     :return: множество uuid всех категорий дата и средняя ценя которых подлежит обновлению.
     """
-    updated_categories = set()
-    import_categories = {u for u, t in uuids.items() if t == schemas.ShopUnitType.CATEGORY}
+    categories_to_update = {u for u, t in uuids.items() if t == schemas.ShopUnitType.CATEGORY}
+
     # Добавление родительских категорий элементов импорта.
-    all_parents = {unit['parent_id'] for unit in units}
+    _parents = {unit['parent_id'] for unit in units}
     # Добавление родительских категорий элементов до импорта.
-    all_parents |= (set((await session.execute(
+    _parents.update((await session.execute(
         select(model.ShopUnit.parent_id).
         where(model.ShopUnit.uuid.in_(uuids))
-    )).scalars()) - {None})
-    start = datetime.now()
+    )).scalars())
+    _parents.discard(None)
+
     stmt = text(
         """
         with recursive r as (
@@ -93,33 +94,17 @@ async def parents(units: List[Dict], uuids: Dict, session: Session) -> Set[UUID]
         ) select uuid from r;
         """
     )
-    ancestors = (await session.execute(
-        stmt.bindparams(bindparam('uuids', expanding=True)),
-        {'uuids': tuple(all_parents)}
-    )).scalars()
-    updated_categories.update(set(ancestors))
-    updated_categories |= import_categories
-    updated_categories.discard(None)
-    stmt = text(
-        """
-        with recursive r as (
-            select uuid, 0 as deep
-            from categories
-            where uuid in :uuids
-            UNION DISTINCT
-            select c.parent_id, r.deep + 1 as deep
-            from r
-            join categories c on c.uuid = r.uuid
-        ) select max(deep), count(1) from r;
-        """
-    )
-    (max_deep, to_be_updated), = (await session.execute(
-        stmt.bindparams(bindparam('uuids', expanding=True)),
-        {'uuids': tuple(all_parents)}
-    )).all()
-    print("find parents:", datetime.now() - start)
-    print(f"{max_deep=} {to_be_updated=}")
-    return updated_categories
+
+    if _parents:
+        # Получаем всех предков вплоть до корня.
+        ancestors = (await session.execute(
+            stmt.bindparams(bindparam('uuids', expanding=True)),
+            {'uuids': tuple(_parents)}
+        )).scalars()
+        categories_to_update.update(ancestors)
+
+    categories_to_update.discard(None)
+    return categories_to_update
 
 
 async def insert_imported_units(
@@ -129,13 +114,8 @@ async def insert_imported_units(
         types: Dict,
         session: Session
 ):
-    # Получаем типы юнитов, которые ВОЗМОЖНО будем обновлять.
-    result = await session.execute(
-        select(model.ShopUnit).
-        where(model.ShopUnit.uuid.in_(types))
-    )
     # Проверяем что не изменяется тип уже существующего юнита.
-    for u, in result.all():
+    for u in (await crud.ShopUnit.get_by_uuids(types, session)):
         if u.type != types[u.uuid]:
             raise ValidationFailed400("it is not allowed to change the unit type")
     # Вставка/обновление категорий.
@@ -168,41 +148,6 @@ async def insert_imported_units(
         await session.execute(insert(model.OffersHistory).values(offers))
 
 
-async def save_updated_stats(updated_stats: dict, update_date: datetime, session: Session):
-    to_save = []
-    updated_categories = (await session.execute(
-        select(model.Category).
-        where(model.Category.uuid.in_(updated_stats))
-    )).scalars()
-    for c in updated_categories:
-        offers_number, total_price = updated_stats[c.uuid]
-        to_save.append({
-            '_uuid': c.uuid,
-            'total_price': total_price,
-            'offers_number': offers_number,
-            'date': update_date,
-            'name': c.name,
-            'parent_id': c.parent_id
-        })
-    await session.execute(insert(model.CategoryHistory).values(
-        uuid=bindparam('_uuid'),
-        total_price=bindparam('total_price'),
-        offers_number=bindparam('offers_number'),
-        date=bindparam('date'),
-        parent_id=bindparam('parent_id'),
-        name=bindparam('name')
-    ), to_save)
-    await session.execute(
-        update(model.Category).
-        where(model.Category.uuid == bindparam('_uuid')).
-        values(
-            date=bindparam('date'),
-            total_price=bindparam('total_price'),
-            offers_number=bindparam('offers_number')
-        ), to_save
-    )
-
-
 async def handle(unit_import: schemas.ShopUnitImportRequest):
     """
     Вставка и валидация данных, полученных от пользователя, в рамках одной транзакции.
@@ -211,40 +156,50 @@ async def handle(unit_import: schemas.ShopUnitImportRequest):
     categorise, offers, units, types = prepare(unit_import)
     async with Session() as s:
         async with s.begin():
-            # await s.connection(execution_options={'isolation_level': 'REPEATABLE READ'})
             try:
-                await insert_imported_units(categorise, offers, units, types, s)
+                start = datetime.now()
                 parent_uuids = await parents(units, uuids=types, session=s)
+                await insert_imported_units(categorise, offers, units, types, s)
                 if not parent_uuids:
                     return
-                start = datetime.now()
                 stmt = text(
                     """
                     with recursive r as (
-                        select uuid as root, uuid as crurent_uuid
+                        select uuid as root, uuid as current_uuid
                         from categories
                         where uuid in :uuids
                         UNION
                         select r.root, c.uuid
                         from r
-                        join categories c on c.parent_id = r.crurent_uuid
-                    ) select r.root, sum(o.price), count(1) from r
-                    join offers o on r.crurent_uuid = o.parent_id
-                    group by r.root;
+                        join categories c on c.parent_id = r.current_uuid
+                    ) update categories c
+                        set total_price = recursive.sum, offers_number = recursive.count,
+                        date = :date
+                        from (
+                            select r.root, sum(o.price)::INT as sum , count(o.uuid) as count from r
+                            left join offers o on r.current_uuid = o.parent_id
+                            group by r.root
+                        ) as recursive where c.uuid = recursive.root
+                        returning c.uuid;
                     """
                 )
-                updated_categories = (
-                    await s.execute(stmt.bindparams(
-                        bindparam('uuids', expanding=True)),
-                        {'uuids': tuple(parent_uuids)}
-                    )).all()
-
-                updated_stats = {}
-                for line in updated_categories:
-                    uuid, total, number = line
-                    updated_stats[uuid] = (number, total)
-                print("calculate:", datetime.now() - start)
-                if updated_stats:
-                    await save_updated_stats(updated_stats, unit_import.update_date, s)
+                updated = (await s.execute(stmt.bindparams(
+                    bindparam('uuids', expanding=True)),
+                    {'uuids': tuple(parent_uuids), 'date': unit_import.update_date}
+                )).scalars()
+                await s.execute(
+                    insert(model.CategoryHistory).
+                    from_select(
+                        ['uuid', 'parent_id', 'date', 'name', 'total_price', 'offers_number'],
+                        select(
+                            model.Category.uuid,
+                            model.Category.parent_id,
+                            model.Category.date,
+                            model.Category.name,
+                            model.Category.total_price,
+                            model.Category.offers_number
+                        ).where(model.Category.uuid.in_(parent_uuids))
+                    ))
+                print("imports time: ", datetime.now() - start)
             except IntegrityError:
                 raise ValidationFailed400("db integrity error")
